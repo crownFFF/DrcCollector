d7294058ff73df6d1f32883a65f9e6f55ceb241a
crownFFF <asd8792323@gmail.com>
2024-08-02 10:49:24 +0800

    commit

---
+++ src/lib/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$dsn = "mysql:host=sql213.byethost7.com;dbname= b7_36819277_collector;charset=utf8";
+$user = "b7_36819277";
+$password = "Aa24619622";
+
+$link = new PDO($dsn, $user, $password);
+
+date_default_timezone_set("Asia/Taipei");
