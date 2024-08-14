dcdb2c583f3a08aa0dce4984149ac02c97e22e76
crownFFF <asd8792323@gmail.com>
2024-08-14 22:19:26 +0800

    commit

--- src/scripts/itemList.js
+++ src/scripts/itemList.js
@@ -101,111 +101,201 @@ $('.lightBoxclose').on('click', (e) => {
 
 const page = window.location.pathname.split('/')
 
-$.ajax({
-  url: 'https://drc-api.hopto.org/api/exhibits-item.php',
-  type: 'POST',
-  contentType: 'application/json',
-}).done(function (data) {
-  const List = data.filter((item) => {
-    return page.includes(item.typeEN)
-  })
+if (page.includes('Stamp')) {
+  $.ajax({
+    url: 'https://drc-api.hopto.org/api/exhibits-stamp.php',
+    type: 'POST',
+    contentType: 'application/json',
+  }).done((data) => {
+    data.forEach(item => {
+      const bigDiv = $('<div>', {
+        class: 'exhibititem'
+      })
+      const smallDiv = $('<div>', {
+        class: 'exhibitTitle'
+      })
+      const img = $('<img>', {
+        src: `https://drccollector.hopto.org/images/stamps/${item.img}`,
+        alt: "",
+        'data-id': `${item.id}`,
+        draggable: "false"
+      })
 
-  List.forEach(item => {
-    const bigDiv = $('<div>', {
-      class: 'exhibititem'
-    })
-    const smallDiv = $('<div>', {
-      class: 'exhibitTitle'
-    })
-    const img = $('<img>', {
-      src: `https://drccollector.hopto.org/images/envelopes/${item.img}`,
-      alt: "",
-      'data-id': `${item.id}`,
-      draggable: "false"
-    })
-
-    const theme = $('<p>')
-    theme.html(item.theme ? `主題:${item.theme}` : '')
-
-    const time = $('<p>')
-    time.html(item.time ? `年份:${item.time}` : '')
-
-    smallDiv.append(theme, time)
-    bigDiv.append(img, smallDiv)
-    $('.exhibitList').append(bigDiv)
-
-  });
-
-  const $lightBoxInfo = $('.lightBoxInfo');
-  const $lightBoxImg = $('.lightBoxImg img');
-  const $secImg = $('.secImg');
-
-  $('.exhibititem img').on('click', (e) => {
-    const id = e.target.dataset.id;
-    $('body').css('overflow', 'hidden')
-    $.ajax({
-      url: 'https://drc-api.hopto.org/api/exhibits-item.php',
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({ id })
-    }).done(function (data) {
-      console.log(data);
-      
-      const item = data[0];
-      updateLightboxInfo(item, data);
-      return loadCountryInfo(item.country_from || '', item.country_to || '');
-    }).fail(function () {
-      alert("系統目前無法連接後台資料庫");
+      const year = $('<p>')
+      year.html(item.year ? `年份:${item.year}` : '年份:無')
+      smallDiv.append(year)
+      bigDiv.append(img, smallDiv)
+      $('.exhibitList').append(bigDiv)
     });
 
-    $('.lightBoxArea').css('display', 'block');
-  });
+    $('.exhibititem img').on('click', (e) => {
+      const id = e.target.dataset.id;
+      $('body').css('overflow', 'hidden')
+      $.ajax({
+        url: 'https://drc-api.hopto.org/api/exhibits-stamp.php',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ id })
+      }).done(function (data) {
+        const item = data[0];
+        const $lightBoxInfo = $('.lightBoxInfo');
+        const $lightBoxImg = $('.lightBoxImg img');
+        const $secImg = $('.secImg');
+        item.price = item.price ? item.price : ''
+        item.unit = item.unit ? item.unit : ''
 
-  function updateLightboxInfo(item, data) {
-    $lightBoxInfo.find('.theme').html(`主題:${item.theme}`);
-    $lightBoxInfo.find('.remark').html(`備註:<br>${item.remark || ''}`);
-    $lightBoxInfo.find('.time').html(`年份:${item.time || ''}`);
-    $lightBoxImg.attr('src', `https://drccollector.hopto.org/images/envelopes/${item.img}`);
+        $lightBoxInfo.find('.theme').html(`面額:${item.price + item.unit}`);
+        $lightBoxInfo.find('.remark').html(`備註:<br>${item.remark || ''}`);
+        $lightBoxInfo.find('.time').html(`年份:${item.year || ''}`);
+        $lightBoxImg.attr('src', `https://drccollector.hopto.org/images/stamps/${item.img}`);
 
-    if (data.length > 1) {
-      $secImg.css('display', 'flex').empty();
-      data.forEach((imgItem) => {
-        const newImg = $('<img>', {
-          src: `https://drccollector.hopto.org/images/envelopes/${imgItem.img}`,
-          alt: "",
-          "data-id": "",
-          draggable: "false",
-          class: "littleImg"
+        if (data.length > 1) {
+          $secImg.css('display', 'flex').empty();
+          data.forEach((imgItem) => {
+            const newImg = $('<img>', {
+              src: `https://drccollector.hopto.org/images/envelopes/${imgItem.img}`,
+              alt: "",
+              "data-id": "",
+              draggable: "false",
+              class: "littleImg"
+            });
+            $secImg.append(newImg);
+          });
+        }
+
+        $('.littleImg').on('click', (e) => {
+          scale = 1;
+          $lightBoxImg.removeAttr('style').attr('src', e.target.src);
         });
-        $secImg.append(newImg);
+
+
+        return loadCountryInfoStamp(item.country_id||'');
+
+        function loadCountryInfoStamp(country_id) {
+          return $.ajax({
+            url: 'https://drc-api.hopto.org/api/exhibits-country.php',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ country_id })
+          }).done(function (data) {
+            $lightBoxInfo.find('.country_id').html(`國家:${data[0].c_name}`);
+          })
+        }
+      }).fail(function () {
+        alert("系統目前無法連接後台資料庫");
+      });
+      $('.lightBoxArea').css('display', 'block');
+    });
+
+  }).fail((data) => {
+    alert("系統目前無法連接後台資料庫");
+  })
+
+} else {
+
+  $.ajax({
+    url: 'https://drc-api.hopto.org/api/exhibits-item.php',
+    type: 'POST',
+    contentType: 'application/json',
+  }).done(function (data) {
+    const List = data.filter((item) => {
+      return page.includes(item.typeEN)
+    })
+
+    List.forEach(item => {
+      const bigDiv = $('<div>', {
+        class: 'exhibititem'
+      })
+      const smallDiv = $('<div>', {
+        class: 'exhibitTitle'
+      })
+      const img = $('<img>', {
+        src: `https://drccollector.hopto.org/images/envelopes/${item.img}`,
+        alt: "",
+        'data-id': `${item.id}`,
+        draggable: "false"
+      })
+
+      const theme = $('<p>')
+      theme.html(item.theme ? `主題:${item.theme}` : '')
+
+      const time = $('<p>')
+      time.html(item.time ? `年份:${item.time}` : '')
+
+      smallDiv.append(theme, time)
+      bigDiv.append(img, smallDiv)
+      $('.exhibitList').append(bigDiv)
+
+    });
+
+    const $lightBoxInfo = $('.lightBoxInfo');
+    const $lightBoxImg = $('.lightBoxImg img');
+    const $secImg = $('.secImg');
+
+    $('.exhibititem img').on('click', (e) => {
+      const id = e.target.dataset.id;
+      $('body').css('overflow', 'hidden')
+      $.ajax({
+        url: 'https://drc-api.hopto.org/api/exhibits-item.php',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ id })
+      }).done(function (data) {
+        const item = data[0];
+        updateLightboxInfo(item, data);
+        return loadCountryInfo(item.country_from || '', item.country_to || '');
+      }).fail(function () {
+        alert("系統目前無法連接後台資料庫");
+      });
+
+      $('.lightBoxArea').css('display', 'block');
+    });
+
+    function updateLightboxInfo(item, data) {
+      $lightBoxInfo.find('.theme').html(`主題:${item.theme}`);
+      $lightBoxInfo.find('.remark').html(`備註:<br>${item.remark || ''}`);
+      $lightBoxInfo.find('.time').html(`年份:${item.time || ''}`);
+      $lightBoxImg.attr('src', `https://drccollector.hopto.org/images/envelopes/${item.img}`);
+
+      if (data.length > 1) {
+        $secImg.css('display', 'flex').empty();
+        data.forEach((imgItem) => {
+          const newImg = $('<img>', {
+            src: `https://drccollector.hopto.org/images/envelopes/${imgItem.img}`,
+            alt: "",
+            "data-id": "",
+            draggable: "false",
+            class: "littleImg"
+          });
+          $secImg.append(newImg);
+        });
+      }
+
+      $('.littleImg').on('click', (e) => {
+        scale = 1;
+        $lightBoxImg.removeAttr('style').attr('src', e.target.src);
       });
     }
 
-    $('.littleImg').on('click', (e) => {
-      scale = 1;
-      $lightBoxImg.removeAttr('style').attr('src', e.target.src);
-    });
-  }
-
-  function loadCountryInfo(country_from, country_to) {
-    return $.ajax({
-      url: 'https://drc-api.hopto.org/api/exhibits-country.php',
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({ country_from, country_to })
-    }).done(function (data) {
-      updateCountryInfo(data);
-    })
-  }
-
-  function updateCountryInfo(data) {
-    const from = data[0]?.c_name || '';
-    const to = data[1]?.c_name || '';
-    $lightBoxInfo.find('.country_from').html(`寄出國家:${from}`);
-    $lightBoxInfo.find('.country_to').html(`收件國家:${to}`);
-  }
+    function loadCountryInfo(country_from, country_to) {
+      return $.ajax({
+        url: 'https://drc-api.hopto.org/api/exhibits-country.php',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ country_from, country_to })
+      }).done(function (data) {
+        const from = data[0]?.c_name || '';
+        const to = data[1]?.c_name || '';
+        $lightBoxInfo.find('.country_from').html(`寄出國家:${from}`);
+        $lightBoxInfo.find('.country_to').html(`收件國家:${to}`);
+      })
+    }
 
 
-}).fail(function () {
-  alert("系統目前無法連接後台資料庫");
-});
+
+
+
+  }).fail(function () {
+    alert("系統目前無法連接後台資料庫");
+  });
+}
